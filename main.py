f67192955aa06b5bcb6ab81c71ea24992cd69e64
evileg0 <30571299+evileg0@users.noreply.github.com>
2025-01-26 12:12:40 +0300

    starting parser

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(dest="command")
+
+# Сабпарсер для копирования файла
+parser_copy = subparsers.add_parser('copy', help="Copy file <from> to <destanation>")
+parser_copy.add_argument('filename', type=str, help="Source file")
+parser_copy.add_argument('destanation', type=str, help="Destanation folder")
+
+args = parser.parse_args()
+print (args)
+
+if args.command == 'copy':
+    print(f"Copying file {args.filename} to {args.destanation}")
+elif args.command == 'delete':
+    print(f"Deleting file...")
+elif args.command == 'count':
+    print(f"Counting files...")
+else:
+    parser.print_help()
+
+
 print("Dz-statrt...")
