b9fc074218a3da424143cc1ef13485c7b1c70f04
evileg0 <30571299+evileg0@users.noreply.github.com>
2025-01-26 13:38:19 +0300

    added 2 more commands

--- main.py
+++ main.py
@@ -9,17 +9,25 @@ parser_copy = subparsers.add_parser('copy', help="Copy file <from> to <destanati
 parser_copy.add_argument('filename', type=str, help="Source file")
 parser_copy.add_argument('destanation', type=str, help="Destanation folder")
 
+# Сабпарсер удаления файла
+parser_copy = subparsers.add_parser('del', help="Delete file")
+parser_copy.add_argument('filename', type=str, help="File name")
+
+# Сабпарсер для подсчета файлов
+parser_copy = subparsers.add_parser('count', help="Count files in directory")
+parser_copy.add_argument('folder', type=str, help="Folder name")
+
 args = parser.parse_args()
 print (args)
 
 if args.command == 'copy':
     print(f"Copying file {args.filename} to {args.destanation}")
-elif args.command == 'delete':
-    print(f"Deleting file...")
+elif args.command == 'del':
+    print(f"Deleting file {args.filename} ...")
 elif args.command == 'count':
-    print(f"Counting files...")
+    print(f"Counting files in {args.folder} ...")
 else:
     parser.print_help()
 
 
-print("Dz-statrt...")
+print("Done!")
